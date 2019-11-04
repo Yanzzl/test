39bbbf170c0d1b2929b2992e1deedfd12a552666
Mei Lan <m.l.schrama@student.utwente.nl>
2019-11-04 14:09:18 +0100

    added all games

--- app/src/main/java/com/example/test/Web.java
+++ app/src/main/java/com/example/test/Web.java
@@ -2,18 +2,52 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.widget.ImageView;
 
 public class Web extends AppCompatActivity {
+
+    int game;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web);
 
+
+        game = getIntent().getIntExtra("GAME", 0);
+
         WebView web = findViewById(R.id.web_game);
         web.getSettings().setJavaScriptEnabled(true);
-        web.loadUrl("file:///android_asset/game_5/index.html");
+
+        switch (game) {
+            case 0:
+                System.out.println("Not received");
+            case 1:
+                web.loadUrl("file:///android_asset/game_1/index.html");
+                break;
+            case 2:
+                web.loadUrl("file:///android_asset/game_2/index.html");
+                break;
+            case 3:
+                web.loadUrl("file:///android_asset/game_3/index.html");
+                break;
+            case 4:
+                web.loadUrl("file:///android_asset/game_4/index.html");
+                break;
+            case 5:
+                web.loadUrl("file:///android_asset/game_5/index.html");
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, game.class);
+        this.finishAffinity();
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/test/game.java
+++ app/src/main/java/com/example/test/game.java
@@ -3,6 +3,7 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
@@ -16,44 +17,52 @@ public class game extends AppCompatActivity {
 
     private UserData userData = UserData.getInstance();
 
+    ImageView g1;
+    ImageView g2;
+    ImageView g3;
+    ImageView g4;
+    ImageView g5;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        ImageView g1 = findViewById(R.id.g1);
+
+        g1 = findViewById(R.id.g1);
         if (userData.getGameState(1)) {
             g1.setImageResource(R.drawable.g1);
         } else {
             g1.setImageResource(R.drawable.g1_locked);
         }
 
-        ImageView g2 = findViewById(R.id.g2);
+        g2 = findViewById(R.id.g2);
         if (userData.getGameState(2)) {
-            g1.setImageResource(R.drawable.g2);
+            g2.setImageResource(R.drawable.g2);
         } else {
-            g1.setImageResource(R.drawable.g2_locked);
+            g2.setImageResource(R.drawable.g2_locked);
         }
 
-        ImageView g3 = findViewById(R.id.g3);
+        g3 = findViewById(R.id.g3);
         if (userData.getGameState(3)) {
-            g1.setImageResource(R.drawable.g3);
+            g3.setImageResource(R.drawable.g3);
         } else {
-            g1.setImageResource(R.drawable.g3_locked);
+            g3.setImageResource(R.drawable.g3_locked);
         }
 
-        ImageView g4 = findViewById(R.id.g4);
+        g4 = findViewById(R.id.g4);
         if (userData.getGameState(4)) {
-            g1.setImageResource(R.drawable.g4);
+            g4.setImageResource(R.drawable.g4);
         } else {
-            g1.setImageResource(R.drawable.g4_locked);
+            g4.setImageResource(R.drawable.g4_locked);
         }
 
-        ImageView g5 = findViewById(R.id.g5);
+        g5 = findViewById(R.id.g5);
         if (userData.getGameState(5)) {
-            g1.setImageResource(R.drawable.g5);
+            g5.setImageResource(R.drawable.g5);
         } else {
-            g1.setImageResource(R.drawable.g5_locked);
+            g5.setImageResource(R.drawable.g5_locked);
         }
 
 
@@ -63,7 +72,9 @@ public class game extends AppCompatActivity {
                 if (userData.getGameState(1)) {
                     userData.unlockGame(2);
                     Intent intent = new Intent(game.this, Web.class);
+                    intent.putExtra("GAME", 1);
                     startActivity(intent);
+
                 } else {
 
                 }
@@ -76,6 +87,7 @@ public class game extends AppCompatActivity {
                 if (userData.getGameState(2)) {
                     userData.unlockGame(3);
                     Intent intent = new Intent(game.this, Web.class);
+                    intent.putExtra("GAME", 2);
                     startActivity(intent);
                 } else {
 
@@ -89,6 +101,7 @@ public class game extends AppCompatActivity {
                 if (userData.getGameState(3)) {
                     userData.unlockGame(4);
                     Intent intent = new Intent(game.this, Web.class);
+                    intent.putExtra("GAME", 3);
                     startActivity(intent);
                 } else {
 
@@ -102,6 +115,7 @@ public class game extends AppCompatActivity {
                 if (userData.getGameState(4)) {
                     userData.unlockGame(5);
                     Intent intent = new Intent(game.this, Web.class);
+                    intent.putExtra("GAME", 4);
                     startActivity(intent);
                 } else {
 
@@ -114,15 +128,20 @@ public class game extends AppCompatActivity {
             public void onClick(View v) {
                 if (userData.getGameState(5)) {
                     Intent intent = new Intent(game.this, Web.class);
+                    intent.putExtra("GAME", 5);
                     startActivity(intent);
                 } else {
 
                 }
             }
         });
+    }
 
-
-
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, MainActivity.class);
+        this.finishAffinity();
+        startActivity(intent);
     }
 
 }
